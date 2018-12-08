793d1ec7cec2ca099905e07b6dc313b0f6d8bfcb
Vo Thanh Quy <Vo Thanh Quy>
2018-12-08 23:15:29 +0700

    New upload

---
+++ Buoi Cuoi_SQL_TiepTheo/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace Buoi_Cuoi_SQL_TiepTheo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_ms = new System.Windows.Forms.TextBox();
+            this.textBox_ho = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_ten = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox_chucvu = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.radioButton_nam = new System.Windows.Forms.RadioButton();
+            this.radioButton_nu = new System.Windows.Forms.RadioButton();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button_them = new System.Windows.Forms.Button();
+            this.button_sua = new System.Windows.Forms.Button();
+            this.button_xoa = new System.Windows.Forms.Button();
+            this.button_thoat = new System.Windows.Forms.Button();
+            this.button_huy = new System.Windows.Forms.Button();
+            this.button_luu = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ma So";
+            // 
+            // textBox_ms
+            // 
+            this.textBox_ms.Location = new System.Drawing.Point(164, 43);
+            this.textBox_ms.Name = "textBox_ms";
+            this.textBox_ms.Size = new System.Drawing.Size(282, 31);
+            this.textBox_ms.TabIndex = 2;
+            // 
+            // textBox_ho
+            // 
+            this.textBox_ho.Location = new System.Drawing.Point(164, 92);
+            this.textBox_ho.Name = "textBox_ho";
+            this.textBox_ho.Size = new System.Drawing.Size(130, 31);
+            this.textBox_ho.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(55, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 26);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Ho Ten";
+            // 
+            // textBox_ten
+            // 
+            this.textBox_ten.Location = new System.Drawing.Point(316, 92);
+            this.textBox_ten.Name = "textBox_ten";
+            this.textBox_ten.Size = new System.Drawing.Size(130, 31);
+            this.textBox_ten.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(526, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 26);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Chuc vu";
+            // 
+            // comboBox_chucvu
+            // 
+            this.comboBox_chucvu.FormattingEnabled = true;
+            this.comboBox_chucvu.Location = new System.Drawing.Point(624, 39);
+            this.comboBox_chucvu.Name = "comboBox_chucvu";
+            this.comboBox_chucvu.Size = new System.Drawing.Size(287, 33);
+            this.comboBox_chucvu.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(55, 144);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 26);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Ho Ten";
+            // 
+            // radioButton_nam
+            // 
+            this.radioButton_nam.AutoSize = true;
+            this.radioButton_nam.Location = new System.Drawing.Point(194, 144);
+            this.radioButton_nam.Name = "radioButton_nam";
+            this.radioButton_nam.Size = new System.Drawing.Size(84, 30);
+            this.radioButton_nam.TabIndex = 10;
+            this.radioButton_nam.TabStop = true;
+            this.radioButton_nam.Text = "Nam";
+            this.radioButton_nam.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_nu
+            // 
+            this.radioButton_nu.AutoSize = true;
+            this.radioButton_nu.Location = new System.Drawing.Point(302, 144);
+            this.radioButton_nu.Name = "radioButton_nu";
+            this.radioButton_nu.Size = new System.Drawing.Size(65, 30);
+            this.radioButton_nu.TabIndex = 11;
+            this.radioButton_nu.TabStop = true;
+            this.radioButton_nu.Text = "Nu";
+            this.radioButton_nu.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(526, 106);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(109, 26);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Ngay sinh";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(651, 101);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(277, 31);
+            this.dateTimePicker1.TabIndex = 13;
+            // 
+            // button_them
+            // 
+            this.button_them.Location = new System.Drawing.Point(978, 46);
+            this.button_them.Name = "button_them";
+            this.button_them.Size = new System.Drawing.Size(127, 47);
+            this.button_them.TabIndex = 14;
+            this.button_them.Text = "Them";
+            this.button_them.UseVisualStyleBackColor = true;
+            this.button_them.Click += new System.EventHandler(this.button_them_Click);
+            // 
+            // button_sua
+            // 
+            this.button_sua.Location = new System.Drawing.Point(978, 99);
+            this.button_sua.Name = "button_sua";
+            this.button_sua.Size = new System.Drawing.Size(127, 47);
+            this.button_sua.TabIndex = 15;
+            this.button_sua.Text = "Sua";
+            this.button_sua.UseVisualStyleBackColor = true;
+            this.button_sua.Click += new System.EventHandler(this.button_sua_Click);
+            // 
+            // button_xoa
+            // 
+            this.button_xoa.Location = new System.Drawing.Point(978, 154);
+            this.button_xoa.Name = "button_xoa";
+            this.button_xoa.Size = new System.Drawing.Size(127, 47);
+            this.button_xoa.TabIndex = 16;
+            this.button_xoa.Text = "Xoa";
+            this.button_xoa.UseVisualStyleBackColor = true;
+            this.button_xoa.Click += new System.EventHandler(this.button_xoa_Click);
+            // 
+            // button_thoat
+            // 
+            this.button_thoat.Location = new System.Drawing.Point(1137, 154);
+            this.button_thoat.Name = "button_thoat";
+            this.button_thoat.Size = new System.Drawing.Size(127, 47);
+            this.button_thoat.TabIndex = 19;
+            this.button_thoat.Text = "Thoat";
+            this.button_thoat.UseVisualStyleBackColor = true;
+            this.button_thoat.Click += new System.EventHandler(this.button_thoat_Click);
+            // 
+            // button_huy
+            // 
+            this.button_huy.Location = new System.Drawing.Point(1137, 99);
+            this.button_huy.Name = "button_huy";
+            this.button_huy.Size = new System.Drawing.Size(127, 47);
+            this.button_huy.TabIndex = 18;
+            this.button_huy.Text = "Huy";
+            this.button_huy.UseVisualStyleBackColor = true;
+            // 
+            // button_luu
+            // 
+            this.button_luu.Location = new System.Drawing.Point(1137, 46);
+            this.button_luu.Name = "button_luu";
+            this.button_luu.Size = new System.Drawing.Size(127, 47);
+            this.button_luu.TabIndex = 17;
+            this.button_luu.Text = "Luu";
+            this.button_luu.UseVisualStyleBackColor = true;
+            this.button_luu.Click += new System.EventHandler(this.button_luu_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(60, 228);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 33;
+            this.dataGridView1.Size = new System.Drawing.Size(1205, 388);
+            this.dataGridView1.TabIndex = 20;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1320, 646);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button_thoat);
+            this.Controls.Add(this.button_huy);
+            this.Controls.Add(this.button_luu);
+            this.Controls.Add(this.button_xoa);
+            this.Controls.Add(this.button_sua);
+            this.Controls.Add(this.button_them);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.radioButton_nu);
+            this.Controls.Add(this.radioButton_nam);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox_chucvu);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox_ten);
+            this.Controls.Add(this.textBox_ho);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox_ms);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_ms;
+        private System.Windows.Forms.TextBox textBox_ho;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_ten;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox_chucvu;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton radioButton_nam;
+        private System.Windows.Forms.RadioButton radioButton_nu;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button button_them;
+        private System.Windows.Forms.Button button_sua;
+        private System.Windows.Forms.Button button_xoa;
+        private System.Windows.Forms.Button button_thoat;
+        private System.Windows.Forms.Button button_huy;
+        private System.Windows.Forms.Button button_luu;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Buoi Cuoi_SQL_TiepTheo/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Buoi_Cuoi_SQL_TiepTheo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection cnn = new SqlConnection(@"Data Source = VTHANHQUD8DD\SQLEXPRESS; Initial Catalog = NhanVien2; Integrated Security = True");
+        SqlDataAdapter daNV;
+
+        SqlDataAdapter daCVu; //chu yeu de load combobox chuc vu len
+
+        DataSet ds = new DataSet();
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string query = "select * from chucvu";
+            daCVu = new SqlDataAdapter(query, cnn);
+            daCVu.Fill(ds, "chucvu");
+            comboBox_chucvu.DataSource = ds.Tables[0];
+
+
+            comboBox_chucvu.DisplayMember = "tenchucvu";
+            comboBox_chucvu.ValueMember = "machucvu";
+
+
+            Refesh();
+
+            //them
+            
+
+            string query_them = "insert into nhanvien values (@maso, @ho,@ten ,@ngaysinh ,@gioitinh,@machucvu)";
+            SqlCommand cmd_them = new SqlCommand(query_them, cnn);
+            cmd_them.Parameters.Add("@maso",SqlDbType.NVarChar,20, "maso");
+            cmd_them.Parameters.Add("@ho", SqlDbType.NVarChar,20, "ho");
+            cmd_them.Parameters.Add("@ten", SqlDbType.NVarChar,20, "ten");
+            cmd_them.Parameters.Add("@ngaysinh", SqlDbType.DateTime,10, "ngaysinh");
+            cmd_them.Parameters.Add("@gioitinh", SqlDbType.NVarChar, 20, "gioitinh");
+            cmd_them.Parameters.Add("@machucvu", SqlDbType.NVarChar, 2, "machucvu");
+
+            daNV.InsertCommand = cmd_them;
+            
+
+
+            
+
+            //xoa
+
+            string query_xoa = "delete from nhanvien where maso=@maso";
+            SqlCommand cmd_xoa = new SqlCommand(query_xoa, cnn);
+            cmd_xoa.Parameters.Add("@maso", SqlDbType.NVarChar, 20, "maso");
+
+            daNV.DeleteCommand = cmd_xoa;
+
+
+            //sua
+            string query_sua = "update NhanVien set maso=@maso, ho=@ho, ten=@ten, ngaysinh=@ngaysinh, gioitinh=@gioitinh, machucvu=@machucvu  where maso=@maso";
+            SqlCommand cmd_sua = new SqlCommand(query_sua, cnn);
+            cmd_sua.Parameters.Add("@maso", SqlDbType.NVarChar, 20, "maso");
+            cmd_sua.Parameters.Add("@ho", SqlDbType.NVarChar, 20, "ho");
+            cmd_sua.Parameters.Add("@ten", SqlDbType.NVarChar, 20, "ten");
+            cmd_sua.Parameters.Add("@ngaysinh", SqlDbType.DateTime, 10, "ngaysinh");
+            cmd_sua.Parameters.Add("@gioitinh", SqlDbType.NVarChar, 20, "gioitinh");
+            cmd_sua.Parameters.Add("@machucvu", SqlDbType.NVarChar, 2, "machucvu");
+
+            daNV.UpdateCommand = cmd_sua;
+
+
+        }
+
+        
+
+        public void Refesh()
+        {
+            string query = @"select nv.*, cv.tenchucvu from NhanVien as nv, ChucVu as cv where nv.machucvu = cv.machucvu ";
+            daNV = new SqlDataAdapter(query, cnn);
+            daNV.Fill(ds, "nhanvien");
+            dataGridView1.DataSource = ds.Tables[1];
+          
+            //dataGridView1.Columns["machucvu"].Visible = false;
+
+
+        }
+        private void button_thoat_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int current = dataGridView1.CurrentCell.RowIndex;
+            DataGridViewRow dr = dataGridView1.Rows[current];
+
+            textBox_ms.Text = dr.Cells["maso"].Value.ToString();
+            textBox_ho.Text = dr.Cells["ho"].Value.ToString();
+            textBox_ten.Text = dr.Cells["ten"].Value.ToString();
+            comboBox_chucvu.SelectedValue = dr.Cells["machucvu"].Value.ToString();
+            dateTimePicker1.Text = dr.Cells["ngaysinh"].Value.ToString();
+
+            if (dr.Cells["gioitinh"].Value.ToString() == "nu")
+                radioButton_nu.Checked = true;
+            else
+                radioButton_nam.Checked = true;
+        }
+
+        private void button_them_Click(object sender, EventArgs e)
+        {
+            DataRow row = ds.Tables["nhanvien"].NewRow();
+            row["maso"] = textBox_ms.Text;
+            row["ho"] = textBox_ho.Text;
+            row["ten"] = textBox_ten.Text;
+            if(radioButton_nam.Checked == true)
+            {
+                row["gioitinh"] = "nam";
+            }
+            else
+            {
+                row["gioitinh"] = "nu";
+            }
+            row["ngaysinh"] = dateTimePicker1.Value;
+            row["machucvu"] = comboBox_chucvu.SelectedValue;
+
+            ds.Tables["nhanvien"].Rows.Add(row);
+        }
+
+        private void button_luu_Click(object sender, EventArgs e)
+        {
+            daNV.Update(ds, "nhanvien");
+            MessageBox.Show("Done !");
+            dataGridView1.Refresh();
+
+        }
+
+        private void button_xoa_Click(object sender, EventArgs e)
+        {
+            DataGridViewRow dgv1 = dataGridView1.SelectedRows[0];
+            dataGridView1.Rows.Remove(dgv1);
+        }
+
+        private void button_sua_Click(object sender, EventArgs e)
+        {
+            int current = dataGridView1.CurrentCell.RowIndex;
+            DataGridViewRow dr = dataGridView1.Rows[current];
+
+            dataGridView1.BeginEdit(true);
+
+            dr.Cells["maso"].Value = textBox_ms.Text;
+            dr.Cells["ho"].Value = textBox_ho.Text;
+            dr.Cells["ten"].Value = textBox_ten.Text;
+            if (radioButton_nam.Checked == true)
+            {
+                dr.Cells["gioitinh"].Value = "nam";
+            }
+            else
+            {
+                dr.Cells["gioitinh"].Value = "nu";
+            }
+            dr.Cells["ngaysinh"].Value = dateTimePicker1.Value;
+            dr.Cells["machucvu"].Value = comboBox_chucvu.SelectedValue;
+
+            dataGridView1.EndEdit();
+
+        }
+    }
+}
